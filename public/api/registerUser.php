5efb1157afc21e260743d1ad54d8154a8af17391
Ashok <ashok05@somaiya.edu>
2019-05-28 21:26:07 -0400

    Fixed request check in registerUser

--- public/api/registerUser.php
+++ public/api/registerUser.php
@@ -7,6 +7,10 @@ $queryArr = new RegisterUser($_POST);
 if ($_POST['request'] == 'register') {
   $queryArr->create();
 }
-else {
+else if ($_POST['request'] == 'log-in') {
   $queryArr->checkUser();
+}
+
+else if ($_POST['request'] == 'log-in-phy') {
+  $queryArr->checkPhysician();
 }
