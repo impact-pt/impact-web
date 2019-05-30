277c84ba20a941e3af9709d00b14f545bb86b477
Ashok <ashok05@somaiya.edu>
2019-05-30 02:06:57 -0400

    -

--- app/models/FetchPatients.php
+++ app/models/FetchPatients.php
@@ -12,10 +12,10 @@ class FetchPatients{
     global $username;
     $db= new PDO(DB_SERVER,DB_USER,DB_PW);
     // $sql = 'SELECT * FROM RegisteredUser';
-    $sql= 'SELECT ru.username, ru.firstName, ru.lastName, ru.email, ru.phone FROM Physician phy, RegisteredUser ru, AssignedPhysician ap WHERE ap.userID = ru.userID AND ap.assignedPhysicianID = phy.physicianID AND phy.username = '$username'';
+    $sql= 'SELECT ru.username, ru.firstName, ru.lastName, ru.email, ru.phone FROM Physician phy, RegisteredUser ru, AssignedPhysician ap WHERE ap.userID = ru.userID AND ap.assignedPhysicianID = phy.physicianID AND phy.username = ?';
     $statement = $db->prepare($sql);
     $success = $statement->execute(
-      // $this->username
+      $this->username
     );
     $arr=[];
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
