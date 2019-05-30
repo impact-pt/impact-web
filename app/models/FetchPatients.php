6c2ad37451e13988c44ee9498dce7afdd2094d8d
Ashok <ashok05@somaiya.edu>
2019-05-29 20:15:57 -0400

    debug - testing $sql

--- app/models/FetchPatients.php
+++ app/models/FetchPatients.php
@@ -10,7 +10,8 @@ class FetchPatients{
 
   public static function fetchPatients() {
     $db= new PDO(DB_SERVER,DB_USER,DB_PW);
-    $sql= 'SELECT ru.username, ru.firstName, ru.lastName, ru.email, ru.phone FROM Physician phy, RegisteredUser ru, AssignedPhysician ap WHERE ap.userID = ru.userID AND ap.assignedPhysicianID = phy.physicianID AND phy.username = ?';
+    $sql = 'SELECT * FROM RegisteredUser';
+    // $sql= 'SELECT ru.username, ru.firstName, ru.lastName, ru.email, ru.phone FROM Physician phy, RegisteredUser ru, AssignedPhysician ap WHERE ap.userID = ru.userID AND ap.assignedPhysicianID = phy.physicianID AND phy.username = ?';
     $statement = $db->prepare($sql);
     $success = $statement->execute([
       $this->username
